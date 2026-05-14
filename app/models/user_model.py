d22db94533f6e415e33fedc398fbdf9403b5e70f
StephanyCanuto <Stephany.canutos25@gmail.com>
2026-05-14 02:37:04 -0300

    ajustes da página de perfil

--- app/models/user_model.py
+++ app/models/user_model.py
@@ -1,14 +1,25 @@
 from app.database import get_connection
-# cursor.execute(comando_sql, parametros)
 
+
+# =========================
+# CREATE CLIENTE
+# =========================
 def create_cliente(data):
+
     conn = get_connection()
     cursor = conn.cursor()
 
     try:
-        
+
         cursor.execute("""
-            INSERT INTO clientes (nome, telefone, email, senha, cpf, rg)
+            INSERT INTO clientes (
+                nome,
+                telefone,
+                email,
+                senha,
+                cpf,
+                rg
+            )
             VALUES (%s, %s, %s, %s, %s, %s)
             RETURNING id
         """, (
@@ -22,8 +33,14 @@ def create_cliente(data):
 
         cliente_id = cursor.fetchone()["id"]
 
+        # ENDEREÇO
         cursor.execute("""
-            INSERT INTO enderecos (cep, numero, complemento, cliente_id)
+            INSERT INTO enderecos (
+                cep,
+                numero,
+                complemento,
+                cliente_id
+            )
             VALUES (%s, %s, %s, %s)
         """, (
             data["cep"],
@@ -35,13 +52,24 @@ def create_cliente(data):
         return conn, cursor, cliente_id
 
     except:
-        cursor.close()
-        conn.close()
         raise
 
+    finally:
+        pass
+
+
+# =========================
+# CREATE DEPENDENTE
+# =========================
 def create_dependente(cursor, data, cliente_id):
+
     cursor.execute("""
-        INSERT INTO dependentes (nome, data_nascimento, parentesco, cliente_id)
+        INSERT INTO dependentes (
+            nome,
+            data_nascimento,
+            parentesco,
+            cliente_id
+        )
         VALUES (%s, %s, %s, %s)
     """, (
         data["nome_dependente"],
@@ -50,9 +78,19 @@ def create_dependente(cursor, data, cliente_id):
         cliente_id
     ))
 
+
+# =========================
+# CREATE PET
+# =========================
 def create_pet(cursor, data, cliente_id):
+
     cursor.execute("""
-        INSERT INTO pets (nome, especie, raca, cliente_id)
+        INSERT INTO pets (
+            nome,
+            especie,
+            raca,
+            cliente_id
+        )
         VALUES (%s, %s, %s, %s)
     """, (
         data["nome_pet"],
@@ -61,70 +99,128 @@ def create_pet(cursor, data, cliente_id):
         cliente_id
     ))
 
+
+# =========================
+# BUSCAR USUÁRIO POR EMAIL
+# =========================
 def find_user_by_email(email):
+
     conn = get_connection()
     cursor = conn.cursor()
 
     try:
-        cursor.execute(
-            "SELECT * FROM clientes WHERE email = %s", (email,)
-        )
+
+        cursor.execute("""
+            SELECT
+
+                c.id,
+                c.nome,
+                c.telefone,
+                c.email,
+                c.senha,
+                c.cpf,
+                c.rg,
+
+                e.cep,
+                e.numero,
+                e.complemento
+
+            FROM clientes c
+
+            LEFT JOIN enderecos e
+            ON e.cliente_id = c.id
+
+            WHERE c.email = %s
+        """, (email,))
 
         usuario = cursor.fetchone()
+
         return usuario
-    
+
     except:
-        cursor.close()
-        conn.close()
         raise
 
+    finally:
+        cursor.close()
+        conn.close()
+
+
+# =========================
+# BUSCAR POR CPF
+# =========================
 def find_user_by_cpf(cpf):
+
     conn = get_connection()
     cursor = conn.cursor()
 
     try:
+
         cursor.execute(
-            "SELECT * FROM clientes WHERE cpf = %s", (cpf,)
+            "SELECT * FROM clientes WHERE cpf = %s",
+            (cpf,)
         )
 
         usuario = cursor.fetchone()
+
         return usuario
-    
+
     except:
-        cursor.close()
-        conn.close()
         raise
 
+    finally:
+        cursor.close()
+        conn.close()
+
+
+# =========================
+# BUSCAR POR RG
+# =========================
 def find_user_by_rg(rg):
+
     conn = get_connection()
     cursor = conn.cursor()
 
     try:
+
         cursor.execute(
-            "SELECT * FROM clientes WHERE cpf = %s", (rg,)
+            "SELECT * FROM clientes WHERE rg = %s",
+            (rg,)
         )
 
         usuario = cursor.fetchone()
+
         return usuario
-    
+
     except:
-        cursor.close()
-        conn.close()
         raise
 
+    finally:
+        cursor.close()
+        conn.close()
+
+
+# =========================
+# BUSCAR POR TELEFONE
+# =========================
 def find_user_by_telefone(tel):
+
     conn = get_connection()
     cursor = conn.cursor()
 
     try:
+
         cursor.execute(
-            "SELECT * FROM clientes WHERE cpf = %s", (tel,)
+            "SELECT * FROM clientes WHERE telefone = %s",
+            (tel,)
         )
 
         usuario = cursor.fetchone()
+
         return usuario
-    
+
     except:
+        raise
+
+    finally:
         cursor.close()
         conn.close()
-        raise
